becba48071ced7a98acf6f4799912c277cddc860
HuangMinShi <z7707092003@gmail.com>
2019-09-17 11:50:25 +0800

    安裝mongoose, 與mongodb連線及新增Record model

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://127.0.0.1/record')
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongoose error!')
+})
+db.once('open', () => {
+  console.log('mongoose connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('1')
