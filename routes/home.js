7bf9bb72873ff3e03ebd79df00730d0049a28d35
HuangMinShi <z7707092003@gmail.com>
2019-09-21 11:21:35 +0800

    Add user authenticated in record and home route

--- routes/home.js
+++ routes/home.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const Record = require('../models/record')
 const categoryList = require('../categoryList.json')
 const { addUp } = require('../libs/comFunc')
+const { authenticated } = require('../config/auth')
 
 //首頁
-router.get('/', (req, res) => {
+router.get('/', authenticated, (req, res) => {
   Record.find()
     .sort({ date: 'desc' })
     .exec((err, records) => {
--- routes/record.js
+++ routes/record.js
@@ -3,24 +3,25 @@ const router = express.Router()
 const Record = require('../models/record')
 const categoryList = require('../categoryList.json')
 const { addUp, date } = require('../libs/comFunc')
+const { authenticated } = require('../config/auth')
 let categoryFilter = ''
 
 
 //列出全部
-router.get('/', (req, res) => {
+router.get('/', authenticated, (req, res) => {
   categoryFilter = ''
   res.redirect('/')
 })
 
 //新增1筆頁面
-router.get('/new', (req, res) => {
+router.get('/new', authenticated, (req, res) => {
   const today = date()
   res.render('new', { today, categoryList })
 
 })
 
 //新增1筆
-router.post('/', (req, res) => {
+router.post('/', authenticated, (req, res) => {
   const newRecord = new Record(req.body)
   newRecord.save(err => {
     if (err) return console.log(err)
@@ -29,7 +30,7 @@ router.post('/', (req, res) => {
 })
 
 //編輯1筆頁面
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', authenticated, (req, res) => {
   Record.findById(req.params.id, (err, record) => {
     if (err) return console.log(err)
     const recordCategory = categoryList[record.category]
@@ -38,7 +39,7 @@ router.get('/:id/edit', (req, res) => {
 })
 
 //編輯1筆
-router.put('/:id/edit', (req, res) => {
+router.put('/:id/edit', authenticated, (req, res) => {
   Record.findById(req.params.id, (err, record) => {
     if (err) return console.log(err)
     Object.assign(record, req.body)
@@ -50,7 +51,7 @@ router.put('/:id/edit', (req, res) => {
 })
 
 //刪除1筆
-router.delete('/:id/delete', (req, res) => {
+router.delete('/:id/delete', authenticated, (req, res) => {
   Record.findById(req.params.id, (err, record) => {
     if (err) return console.log(err)
     return record.remove(err => {
@@ -61,7 +62,7 @@ router.delete('/:id/delete', (req, res) => {
 })
 
 //篩選類別
-router.get('/filter', (req, res) => {
+router.get('/filter', authenticated, (req, res) => {
   categoryFilter = req.query.category
   Record.find({ category: categoryFilter })
     .sort({ date: 'desc' })
