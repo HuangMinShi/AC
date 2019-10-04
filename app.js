263219fae4bcc8d8e7f062503a251dab333b6e98
HuangMinShi <z7707092003@gmail.com>
2019-10-04 19:43:04 +0800

    Add user routes and views for register and login pages

--- app.js
+++ app.js
@@ -10,12 +10,35 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+
+// routes
+// 首頁
 app.get('/', (req, res) => {
-  console.log(req.body)
   res.send('Hello world!')
+})
+// 註冊
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+// 註冊提交
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+// 登入
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// 登入提交
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.redirect('/users/login')
 })
 
 app.listen(port, () => {
