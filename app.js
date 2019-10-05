dc8c9fde1a8f63bf4ce0b0a5aea69686e1d93a4d
HuangMinShi <z7707092003@gmail.com>
2019-10-05 13:33:02 +0800

    Add mysql database

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -26,7 +30,13 @@ app.get('/users/register', (req, res) => {
 })
 // 註冊提交
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }).then(user => {
+    res.redirect('/')
+  })
 })
 // 登入
 app.get('/users/login', (req, res) => {
