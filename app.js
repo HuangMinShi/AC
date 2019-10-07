bf2e3b199a33db48b174bcb2a2e998d707860d6d
HuangMinShi <z7707092003@gmail.com>
2019-10-07 20:09:41 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOveride = require('method-override')
+const app = express()
+
+// server 相關變數
+const port = 3000
+
+app.use(methodOveride('_method'))
+
+// 列出全部 Todo
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+// 新增一筆 Todo 頁面
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+// 顯示一筆 Todo 的詳細內容
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+// 新增一筆  Todo
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.delete('/:id/delete', (req, res) => {
+  res.send('刪除 Todo')
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
