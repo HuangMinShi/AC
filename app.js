2145b09adef0804ff57ef66257437c3ba5ef8d17
HuangMinShi <z7707092003@gmail.com>
2019-10-28 20:25:29 +0800

    Initalize project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const exphbs = require('express-handlebars')
+
+const port = 3000
+const app = express()
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`App is running on localhost:${port}`)
+})
