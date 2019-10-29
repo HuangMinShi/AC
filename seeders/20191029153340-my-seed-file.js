4cabd3174fbf0118d65b04c1ae6215563d2f82a5
HuangMinShi <z7707092003@gmail.com>
2019-10-30 00:03:18 +0800

    Install faker and add restaurant seed data

--- seeders/20191029153340-my-seed-file.js
+++ seeders/20191029153340-my-seed-file.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const bcrypt = require('bcryptjs')
+const faker = require('faker')
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [
+    queryInterface.bulkInsert('Users', [
       {
         email: 'root@example.com',
         password: bcrypt.hashSync('1', bcrypt.genSaltSync(10), null),
@@ -29,9 +30,26 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       }], {})
+
+    return queryInterface.bulkInsert('Restaurants',
+      Array
+        .from({ length: 50 })
+        .map(d =>
+          ({
+            name: faker.name.findName(),
+            tel: faker.phone.phoneNumber(),
+            address: faker.address.streetAddress(),
+            opening_hours: '08:00',
+            image: faker.image.imageUrl(),
+            description: faker.lorem.text(),
+            createdAt: new Date(),
+            updatedAt: new Date()
+          })
+        ), {})
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', null, {})
+    queryInterface.bulkDelete('Users', null, {})
+    return queryInterface.bulkDelete('Restaurants', null, {})
   }
 };
