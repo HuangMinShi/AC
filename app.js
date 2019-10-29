3370e6b5dd7fd17de81194f2dcb75747691ffc53
HuangMinShi <z7707092003@gmail.com>
2019-10-30 00:45:37 +0800

    Listen to process.env.PORT

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const flash = require('connect-flash')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
 
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
