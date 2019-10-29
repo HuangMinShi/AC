023f1c8b3c1dc767cd1ffd068c5a4d883b6c5ebf
HuangMinShi <z7707092003@gmail.com>
2019-10-29 11:13:47 +0800

    Confirm user password and uniqle email

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const db = require('./models')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const port = 3000
 const app = express()
@@ -10,6 +12,17 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'swedtuj',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.succcess_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
