c2a99487e424465344acf64145d3a38dade03c00
HuangMinShi <z7707092003@gmail.com>
2019-10-31 14:43:14 +0800

    Add public/css to style table list

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
 app.use('/upload', express.static(__dirname + '/upload'))
+app.use(express.static('public'))
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg')
   res.locals.error_msg = req.flash('error_msg')
