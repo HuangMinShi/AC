5cb39ef66861277331c4155e7c2e53b0bb025593
HuangMinShi <z7707092003@gmail.com>
2019-11-04 11:44:11 +0800

    Add handlebar helper ifCond

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const db = require('./models')
 const port = process.env.PORT || 3000
 const app = express()
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helpers')
+}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
