e4348c9a54a477f1e997686b764353cde59730f3
HuangMinShi <z7707092003@gmail.com>
2019-11-18 20:20:40 +0800

    #2 測試檔案外函式

--- test/test.js
+++ test/test.js
@@ -5,8 +5,16 @@ function sum(a, b) {
 }
 
 // #1 測試檔案內函式
-describe('Sum', function () {
+describe('#1 測試檔案內函式', function () {
   it('sum(1,2) === 3 ?', function () {
     assert.equal(sum(1, 2), 3)
   })
 })
+
+// #2 測試檔案外函式
+describe('#2 測試檔案外函式', function () {
+  it('sum(1,2) === 3 ?', function () {
+    assert.equal(helper.sum(1, 2), 3)
+  })
+})
+*/
