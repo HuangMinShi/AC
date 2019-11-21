5d5093f99022737854e61a67cf12ba7502c294a7
HuangMinShi <z7707092003@gmail.com>
2019-11-21 18:41:45 +0800

    Test POST /signup

--- tests/controllers/userController.test.js
+++ tests/controllers/userController.test.js
@@ -14,9 +14,64 @@ describe('#1 signup', () => {
       .get('/signup')
       .end((err, res) => {
         res.statusCode.should.be.equal(200)
-        res.text.should.be.contains('Sign Up')
+        res.text.should.be.contain('Sign Up')
         done()
       })
   })
 
 })
+
+describe('#2 signup', () => {
+
+  beforeEach(() => {
+    User.destroy({ where: {}, truncate: true })
+  })
+
+  it('POST /signup', (done) => {
+    request(app)
+      .post('/signup')
+      .send('name=name&email=email&password=password&passwordCheck=password')
+      .end((err, res) => {
+        User
+          .findOne({ where: { email: 'email' } })
+          .then(user => {
+            user.email.should.be.equal('email')
+            done()
+          })
+          .catch(err => console.log(err))
+      })
+  })
+
+  it('POST /signup but password !== passwordCheck', (done) => {
+    request(app)
+      .post('/signup')
+      .send('name=name&email=email&password=a&passwordCheck=b')
+      .end((err, res) => {
+        res.statusCode.should.be.equal(302)
+        res.text.should.be.contain('/signup')
+        done()
+      })
+  })
+
+  it('POST /signup but user has existed', (done) => {
+    User
+      .create({ email: 'email' })
+      .then(() => {
+        request(app)
+          .post('/signup')
+          .send('name=name&email=email&password=password&passwordCheck=password')
+          .end((err, res) => {
+            res.statusCode.should.be.equal(302)
+            res.text.should.be.contain('/signup')
+            done()
+          })
+      })
+      .catch(err => console.log(err))
+  })
+
+  afterEach(() => {
+    User.destroy({ where: {}, truncate: true })
+  })
+
+})
+
