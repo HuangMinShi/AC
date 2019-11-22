c03438d2f37b1245fedc3dabacab988c9584aa3a
HuangMinShi <z7707092003@gmail.com>
2019-11-22 21:50:33 +0800

    Test POST api/signin

--- tests/controllers/api/userController.test.js
+++ tests/controllers/api/userController.test.js
@@ -67,4 +67,92 @@ describe('# User controller api', () => {
     })
 
   })
+
+  describe('POST /api/signin', () => {
+
+    before(async () => {
+      await User.destroy({ where: {}, truncate: true })
+
+      // 先註冊
+      await request(app)
+        .post('/api/signup')
+        .send('name=name&email=email&password=password&passwordCheck=password')
+    })
+
+    after(async () => await User.destroy({ where: {}, truncate: true }))
+
+    it('[O] 登入成功', (done) => {
+      request(app)
+        .post('/api/signin')
+        .send('email=email&password=password')
+        .expect(200)
+        .end((err, res) => {
+
+          // 成功回傳資料
+          res.body.status.should.be.equal('success')
+          res.body.token.length.should.be.above(0)
+          err ? done(err) : done()
+        })
+    })
+
+    it('[X] 未輸入必填欄位', (done) => {
+      request(app)
+        .post('/api/signin')
+        .send('')
+        .expect(200)
+        .end((err, res) => {
+
+          // 條件一 回傳資料狀態顯示失敗
+          res.body.status.should.be.equal('failure')
+          // 條件二：檢查回傳資料訊息
+          res.body.message.should.be.equal('需要欄位不存在')
+          err ? done(err) : done()
+        })
+    })
+
+    it('[X] 找不到用戶', (done) => {
+      request(app)
+        .post('/api/signin')
+        .send('email=email2&password=password')
+        .expect(401)
+        .end((err, res) => {
+
+          // 條件一 回傳資料狀態顯示失敗
+          res.body.status.should.be.equal('failure')
+          // 條件二：檢查回傳資料訊息
+          res.body.message.should.be.equal('找不到使用者')
+          err ? done(err) : done()
+        })
+    })
+
+    it('[X] 輸入密碼錯誤', (done) => {
+      request(app)
+        .post('/api/signin')
+        .send('email=email&password=password2')
+        .expect(401)
+        .end((err, res) => {
+
+          // 條件一 回傳資料狀態顯示失敗
+          res.body.status.should.be.equal('failure')
+          // 條件二：檢查回傳資料訊息
+          res.body.message.should.be.equal('輸入密碼錯誤')
+          err ? done(err) : done()
+        })
+    })
+
+    it('[X] 未註冊，直接登入', (done) => {
+      request(app)
+        .post('/api/signin')
+        .send('email=email2&password=password2')
+        .expect(401)
+        .end((err, res) => {
+
+          // 條件一 回傳資料狀態顯示失敗
+          res.body.status.should.be.equal('failure')
+          // 條件二：檢查回傳資料訊息
+          should.equal(res.body.token, undefined)
+          err ? done(err) : done()
+        })
+    })
+  })
 })
